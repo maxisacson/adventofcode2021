d267bf4b23752d12a8d8f91ccb9bbc57a4bc01c0
Max Isacson <5937020+maxisacson@users.noreply.github.com>
2021-12-16 22:11:45 +0100

    Day 15

--- src/main.rs
+++ src/main.rs
@@ -13,6 +13,7 @@ mod day11;
 mod day12;
 mod day13;
 mod day14;
+mod day15;
 
 fn main() {
     day1::run();
@@ -29,5 +30,6 @@ fn main() {
     day12::run();
     day13::run();
     day14::run();
+    day15::run();
 }
 
